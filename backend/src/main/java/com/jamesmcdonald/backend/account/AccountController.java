8f9ad0fb5c2da9ac522097bfd19051d5cb5387e7
James McDonald <jamesmcdonald112@yahoo.ie>
2025-07-27 16:38:44 +0100

    feat: add AccountController and tests for GET, POST, DELETE with global error handling

--- backend/src/main/java/com/jamesmcdonald/backend/account/AccountController.java
+++ backend/src/main/java/com/jamesmcdonald/backend/account/AccountController.java
@@ -1,4 +1,37 @@
 package com.jamesmcdonald.backend.account;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/accounts")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return this.accountService.getAllAccounts();
+    }
+
+    @PostMapping
+    public Account createAccount() {
+        return this.accountService.createAndSaveAccount();
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAccount(@PathVariable @Min(1) Long id) {
+        this.accountService.deleteAccountById(id);
+    }
+
 }
