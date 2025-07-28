8373c7bf83e8f5690b478a1f5a5ff2ce668480e1
James McDonald <jamesmcdonald112@yahoo.ie>
2025-07-28 17:00:13 +0100

    feat: add method to find account bby card number and pin

--- backend/src/main/java/com/jamesmcdonald/backend/account/AccountRepository.java
+++ backend/src/main/java/com/jamesmcdonald/backend/account/AccountRepository.java
@@ -2,5 +2,9 @@ package com.jamesmcdonald.backend.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByCardNumberAndPin(String cardNumber, String pin);
 }
