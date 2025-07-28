3fc784dc567a8a621ae716a314b49865f26c831a
James McDonald <jamesmcdonald112@yahoo.ie>
2025-07-28 16:58:50 +0100

    feat: add error messages for account and login validation

--- backend/src/main/java/com/jamesmcdonald/backend/constants/ErrorMessages.java
+++ backend/src/main/java/com/jamesmcdonald/backend/constants/ErrorMessages.java
@@ -2,4 +2,5 @@ package com.jamesmcdonald.backend.constants;
 
 public class ErrorMessages {
     public static final String ACCOUNT_NOT_FOUND = "Account not found";
+    public static final String INVALID_CARD_OR_PIN = "Invalid card number or PIN";
 }
