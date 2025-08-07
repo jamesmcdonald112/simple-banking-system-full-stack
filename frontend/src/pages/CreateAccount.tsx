de25d15c4232491fb683384f6d91eb962d21575c
James McDonald <jamesmcdonald112@yahoo.ie>
2025-08-07 12:56:56 +0100

    Refactor CreateAccount page to use new inputs and styles

--- frontend/src/pages/CreateAccount.tsx
+++ frontend/src/pages/CreateAccount.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import type { Account } from "../types/Account";
 import { createAccount } from "../api/createAccount";
+import TextInput from "../components/TextInput";
+import { MdEmail, MdPerson, MdPhone, MdLock } from 'react-icons/md'
+import PhoneInput from "../components/PhoneInput";
+import EmailInput from "../components/EmailInput";
+import PasswordInput from "../components/PasswordInput";
+import FormButton from "../components/FormButton";
+import FormContainer from "../components/FormContainer";
+
 
 
 export default function CreateAccount() {
@@ -10,6 +18,10 @@ export default function CreateAccount() {
   const[isError, setIsError] = useState<boolean>(false)
   const [errorMessage, setErrorMessage] = useState<string>('')
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [name, setName] = useState<string>('')
+  const [phone, setPhone] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
 
   // Hanlde form submission
   async function handleSubmit(e: React.FormEvent) {
@@ -45,24 +57,61 @@ export default function CreateAccount() {
     
 //  Render Component
  return (
-    <div 
-      className="
-        mx-auto max-w-md 
-        p-6 
-        border-2 border-orange-600 rounded-xl
-        ">
-      <h1>Create Account</h1>
-      <form onSubmit={handleSubmit}>
-        <button 
-          disabled={isSubmitting} 
-          type="submit">
-            {isSubmitting ? "Creating..." : "Create Account"}
-        </button>
+    <FormContainer>
+      <h1 className="text-white text-2xl tracking-wide">Create Account</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-8">
+
+        {/* Name */}
+        <TextInput 
+          label="Full Name"
+          name="fullName"
+          value={name}
+          icon={<MdPerson/>}
+          onChange={(e) => setName(e.target.value)}
+          required={true}
+        />
+
+        {/* Phone */}
+        <PhoneInput 
+          label="Phone Number"
+          name="phoneNumber"
+          value={phone}
+          icon={<MdPhone />}
+          onChange={(e) => setPhone(e.target.value)}
+          required={true}
+        />
+
+        {/* Email */}
+        <EmailInput 
+          label="Email"
+          name="email"
+          value={email}
+          icon={<MdEmail />}
+          onChange={(e) => setEmail(e.target.value)}
+          required={true}
+        />
+
+        {/* Password */}
+        <PasswordInput
+          label="Password"
+          name="password"
+          value={password}
+          icon={<MdLock />}
+          onChange={(e) => setPassword(e.target.value)}
+          required={true}
+        />
+
+        {/* Submit Button */}
+        <FormButton
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "Creating..." : "Create Account"}
+        </FormButton>        
       </form>
 
       {successMessage}
 
       {errorFeedback}   
-    </div>
+    </FormContainer>
  )
 }
