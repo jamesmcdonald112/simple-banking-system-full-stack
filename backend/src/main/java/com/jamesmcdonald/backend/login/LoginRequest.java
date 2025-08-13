a26433bef0e52ad475b88396bc0b16f92dfa1a6a
James McDonald <jamesmcdonald112@yahoo.ie>
2025-08-13 14:44:45 +0100

    refactor(login): convert LoginRequest to record and update usages

--- backend/src/main/java/com/jamesmcdonald/backend/login/LoginRequest.java
+++ backend/src/main/java/com/jamesmcdonald/backend/login/LoginRequest.java
@@ -1,41 +1,7 @@
 package com.jamesmcdonald.backend.login;
 
-public class LoginRequest {
-
-    private String cardNumber;
-    private String pin;
-    private String password;
-
-    public LoginRequest() {
-    }
-
-    public LoginRequest(String cardNumber, String pin, String password) {
-        this.cardNumber = cardNumber;
-        this.pin = pin;
-        this.password = password;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public String getPin() {
-        return pin;
-    }
-
-    public void setPin(String pin) {
-        this.pin = pin;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+public record LoginRequest(
+        String cardNumber,
+        String pin,
+        String password
+) { }
