bb9d57eb353cea05043008ae2d54888b8b585a24
James McDonald <jamesmcdonald112@yahoo.ie>
2025-08-18 11:23:21 +0100

    feat(ui): add Balance component styled like Revolut closes #34

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import WelcomeHeader from "../components/WelcomeHeader";
 import { useAuth } from "../context/AuthContext";
 import Container from "../components/Container";
 import CreditCard from "../components/CreditCard";
+import Balance from "../components/Balance";
 
 export default function Dashboard() {
   const {account, logOut} = useAuth();
@@ -16,10 +17,17 @@ export default function Dashboard() {
   return (
     <Container>
       <WelcomeHeader  name={account.name}/>
+
       <CreditCard 
         cardNumber={account.cardNumber}
         expiry="08/28"
         cvv="6986"
+      />
+
+      <Balance 
+        accountType="Personal" 
+        currency="EUR" 
+        balance={100.37}
       />
       <button onClick={logOut}>Log Out</button>
     </Container>
